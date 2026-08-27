824afbffee5a7f6df3e346251a8b1117c8a34aa0
Meezy642 <meezyvaisex@gmail.com>
2026-08-27 12:13:13 +0700

    Auto-logout and protect all admin routes when session cookie is removed

--- admin/__init__.py
+++ admin/__init__.py
@@ -1,6 +1,15 @@
-from flask import Blueprint, redirect, url_for
+from flask import Blueprint, redirect, url_for, session, request, flash
+from models.user import User
 
 admin_bp = Blueprint('admin', __name__, url_prefix='/admin')
+
+@admin_bp.before_request
+def require_admin_login():
+    if request.endpoint in ['admin_login', 'admin_logout', 'static']:
+        return None
+    if not session.get('username'):
+        flash("Session expired or removed. Please log in to access the admin portal.", "danger")
+        return redirect(url_for('admin_login'))
 
 @admin_bp.route('/')
 def admin_root():
